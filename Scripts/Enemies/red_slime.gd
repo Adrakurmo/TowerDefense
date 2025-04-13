4dc3750e411cf81ce69e597fcf6b01e72eb987f1
DesantBucie <jakub.truszkowski0@hotmail.com>
2025-04-13 23:28:53 +0200

    Added red_slime. Graphics to be changed when ready.

---
+++ Scripts/Enemies/red_slime.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+# NECESSARY VARIABLES
+@export var speed = 700
+@onready var current_path
+@onready var health : float = 100
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var healthbar: ProgressBar = $Healthbar
+
+var CONST_HEALTH : float = 100
+
+func _ready() -> void:
+	current_path = get_parent()
+
+func _process(delta: float) -> void:
+	BEB.set_route_progress(current_path, speed, delta, self)
+
+func boost(duration: float):
+	speed = 900
+	await get_tree().create_timer(duration).timeout
+	speed = 700
+	
+func apply_damage(dmg):
+	health = BEB.set_health_and_apply_damage(dmg, health, CONST_HEALTH, self, sprite_2d)
+	healthbar.set_healthbar((health / CONST_HEALTH) * 100)
+	boost(1)
